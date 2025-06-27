12399426212608b7eeb1bace61eff8cbb281177d
jeong <jeong@gafs.co.jp>
2025-06-27 09:35:21 +0900

    docs: docstring 作成

--- src/excel_to_py/excel_module.py
+++ src/excel_to_py/excel_module.py
@@ -1,8 +1,30 @@
+# excel_module.py
+"""
+openpyxlを利用し、Excelファイルを読み込むモジュール。
+
+Classes:
+    ExcelData: Excelファイルを読み込むクラス
+    DataHandler: 読み込んだExcelDataを操作するクラス
+"""
+
 import openpyxl
 import os
 import sys
 
 class ExcelData:
+    """
+    Excelファイルを読み込む。
+
+    file_pathが指定されていない場合、load()メソッドを使用してファイルの読み込みが必要になります。
+    
+    Attributes:
+        workbook (openpyxl.Workbook): 読み込んだExcelファイルのワークブックオブジェクト
+        sheetnames (list): 読み込んだExcelファイルのシート名のリスト
+        sheets (dict): シート名をキーとし、シートオブジェクトを値とする辞書
+    
+    Methods:
+
+    """
     def __init__(self, file_path = None):
         self.workbook = None
         self.sheetnames = []
@@ -21,6 +43,9 @@ class ExcelData:
         return self.sheets[sheet_name]
 
     def load(self, file_path):
+        """
+        Excelファイルを読み込む
+        """
         if not os.path.exists(file_path):
             raise FileNotFoundError(f"指定されたファイル'{file_path}'が存在していません。")
         
@@ -33,61 +58,96 @@ class ExcelData:
 
 
 class DataHandler:
+    """
+    読み込んだExcelDataを操作するクラス。
+    
+    Attributes:
+        data (ExcelData): ExcelDataのインスタンス。
+
+    Methods:
+        set_sheet(sheet_name): インスタンスのシート名を設定、set_sheet()を使用して、シート名を設定すると、以降のメソッドでsheet_nameを入力しなくても、そのシート名をデフォルトとして使用します。
+        get_sheet(sheet_name): シートのデータを取得します。
+        get_cell(cell, sheet_name): セルの値を取得します。
+        get_row(row, sheet_name): 行のデータを取得します。
+        get_column(column, sheet_name): 列のデータを取得します。
+    """
     def __init__(self, excel_data, sheet_name = None):
         self.data = excel_data
+        # ExcelDataのインスタンスであることを確認
         if not isinstance(self.data, ExcelData):
             raise ValueError("ExcelFileのインスタンスではありません。ExcelFileのインスタンスを渡してください。")
         self._current_sheet_name = None
 
+        # 初期化時にシート名が指定された場合
         if sheet_name is not None:
+            # シートの存在を確認
             if sheet_name not in self.data.sheetnames:
                 raise ValueError(f"シート名 '{sheet_name}' が存在していません。")
             self.set_sheet(sheet_name)
 
     def _resolve_sheet_name(self, sheet_name):
+        """
+        使うシート名を決定する
+        """
+        # 関数実行時、シート名を指定しなかった場合
         if sheet_name is None:
+            # 関数実行時、シート名が指定されていないばあい
             if self._current_sheet_name is None:
+                # set_sheet() でシートめが設定されていない    
                 raise ValueError(f"set_sheet('sheet_name')の実行、または {sys._getframe(1).f_code.co_name}(sheet_name = 'sheet_name')を指定してください")
+            # set_sheet()　でシート名が設定されている場合
             return self._current_sheet_name
+        
+        # 関数実行時、シート名を指定した場合
         if sheet_name not in self.data.sheetnames:
+            # 指定されたシート名が存在しない場合
             raise ValueError(f"シート名 '{sheet_name}' が存在していません。")
+        
+        # 指定されたシート名が存在する場合
         return sheet_name
 
     def set_sheet(self, sheet_name) -> None:
         """
-        シート名を設定
+        インスタンスのシート名を設定
         """
         if sheet_name not in self.data.sheetnames:
-            raise ValueError(f"シート名 '{sheet_name}' が存在しません。")
+            raise ValueError(f"シート名 '{sheet_name}' が存在していません。")
         self._current_sheet_name = sheet_name
 
     def get_sheet(self, sheet_name = None):
         """
-        シート名でシートを取得
+        シート名からシートデータを取得
         """
-        sheet_name = self._resolve_sheet_name(sheet_name)
+        solved_sheet_name = self._resolve_sheet_name(sheet_name)
         sheet_data = []
-        for i in self.data[sheet_name].iter_rows(values_only=True):
+        for i in self.data[solved_sheet_name].iter_rows(values_only=True):
             sheet_data.append(i)
         return sheet_data
 
     def get_cell(self, cell, sheet_name=None):
         """
+        セルの値を取得
         """
-        sheet_name = self._resolve_sheet_name(sheet_name)
-        return self.data[sheet_name][cell].value
+        solved_sheet_name = self._resolve_sheet_name(sheet_name)
+        return self.data[solved_sheet_name][cell].value
     
     def get_row(self, row, sheet_name=None):
-        sheet_name = self._resolve_sheet_name(sheet_name)
+        """ 
+        行のデータを取得
+        """
+        solved_sheet_name = self._resolve_sheet_name(sheet_name)
         if row < 1:
             raise ValueError("行番号は1以上でなければなりません。")
-        return [cell.value for cell in self.data[sheet_name][row]]
+        return [cell.value for cell in self.data[solved_sheet_name][row]]
     
     def get_column(self, column, sheet_name=None):
-        sheet_name = self._resolve_sheet_name(sheet_name)
+        """
+        列のデータを取得
+        """
+        solved_sheet_name = self._resolve_sheet_name(sheet_name)
         if column < 1:
             raise ValueError("列番号は1以上でなければなりません。")
         column_data = []
-        for cell_tuple in self.data[sheet_name].iter_cols(min_col=column, max_col=column, values_only=True):
-            column_data.extend(cell_tuple) # iter_cols는 튜플의 튜플을 반환할 수 있으므로 extend 사용
+        for cell_tuple in self.data[solved_sheet_name].iter_cols(min_col=column, max_col=column, values_only=True):
+            column_data.extend(cell_tuple) # iter_colsはTupleのTupleを返すため、extendを使用
         return column_data
